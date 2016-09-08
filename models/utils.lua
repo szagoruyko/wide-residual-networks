ea025180fdcd8914845c4711657ba34c6d9c6b38
Sergey Zagoruyko <zagoruyko2@gmail.com>
2016-09-08 15:19:55 +0200

    fix MSRA init

--- models/utils.lua
+++ models/utils.lua
@@ -2,7 +2,7 @@ local utils = {}
 
 function utils.MSRinit(model)
    for k,v in pairs(model:findModules('nn.SpatialConvolution')) do
-      local n = v.kW*v.kH*v.nOutputPlane
+      local n = v.kW*v.kH*v.nInputPlane
       v.weight:normal(0,math.sqrt(2/n))
       if v.bias then v.bias:zero() end
    end
